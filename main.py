bebaaf6cd3abec6600735e1bf8fe6e133dd57e16
Jack Coble <jack@coble.uk>
2020-10-12 17:44:14 +0100

    Experimentation with streaming tweets

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+import tweepy
 import os
 from dotenv import load_dotenv
 
-# Load .env file
+# Load .env file and associated vars
 load_dotenv()
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
 
-print(os.getenv("BEARER_TOKEN"))
+# Authentication
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+# Begin streaming
+class StreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print(status.text)
+
+streamListener = StreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=streamListener)
+
+myStream.filter(track=['#covid19'])
